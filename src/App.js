d74a5d2b6c8502c66e165d5394bc49d55012996f
RashadXalil <rashadmkh@code.edu.az>
2022-10-01 23:32:47 +0400

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table, Form, Button, Input } from 'antd'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import fetchUniversities from './api/getUniversities'
+import {
+  getUniversities,
+  getUniversitiesPending,
+  getUniversitiesError,
+} from './redux/reducers/university.reducer'
+import { Spin } from 'antd'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+  const [form] = Form.useForm()
+  const universities = useSelector(getUniversities)
+  const pending = useSelector(getUniversitiesPending)
+  const error = useSelector(getUniversitiesError)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    fetchUniversities()(dispatch)
+  }, [dispatch])
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Country',
+      dataIndex: 'country',
+      key: 'country',
+    },
+    {
+      title: 'Alpha Code',
+      dataIndex: 'alpha_two_code',
+      key: 'alpha_two_code',
+    },
+    {
+      title: 'University Website',
+      dataIndex: 'web_pages',
+      key: 'web_pages',
+      render: (text) => (
+        <a target="_blank" rel="noreferrer" href={`${text}`}>
+          {text}
         </a>
-      </header>
-    </div>
-  );
+      ),
+    },
+  ]
+
+  const submitSearch = (values) => {
+    fetchUniversities(values.university)(dispatch)
+    form.resetFields()
+  }
+
+  if (error) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <div>
+          <h1 style={{ color: 'red', fontWeight: 'bold' }}>
+            Error has occurred...
+          </h1>
+          <h1 style={{ textAlign: 'center', fontWeight: 'bolder' }}>︺︹︺</h1>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1
+          style={{
+            textAlign: 'center',
+            marginBottom: '30px',
+            fontWeight: 'bolder',
+          }}
+        >
+          Universities
+        </h1>
+        <Form
+          form={form}
+          name="search"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={submitSearch}
+          autoComplete="off"
+        >
+          <Form.Item
+            style={{ display: 'block' }}
+            label="University Name"
+            name="university"
+            rules={[
+              {
+                required: true,
+                message: 'University name is required!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button className="searchBtn" htmlType="submit">
+              Search
+            </Button>
+          </Form.Item>
+        </Form>
+        {/* Loading Data */}
+        {pending ? (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Spin size="large" />
+          </div>
+        ) : (
+          <Table rowKey={'name'} dataSource={universities} columns={columns} />
+        )}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/api/getUniversities.js
@@ -0,0 +1,26 @@
+import {
+  fetchUniversitiesError,
+  fetchUniversitiesPending,
+  fetchUniversitiesSuccess,
+} from '../redux/actions/university.actions'
+
+function fetchUniversities(searchValue) {
+  return function (dispatch) {
+    dispatch(fetchUniversitiesPending())
+    let baseURL = 'http://universities.hipolabs.com/search'
+    if (searchValue) {
+      baseURL = `${baseURL}?name=${searchValue}`
+    }
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(fetchUniversitiesSuccess(data))
+        return data
+      })
+      .catch((error) => {
+        dispatch(fetchUniversitiesError(error))
+      })
+  }
+}
+
+export default fetchUniversities
